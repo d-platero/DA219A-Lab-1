3418eceb60b74672831e7728c1412d7ab382362f
elias-da <99768891+elias-da@users.noreply.github.com>
2023-04-23 18:38:07 +0200

    Fix POST route for one album

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const db = require('./model')
 const mongoose = require("mongoose");
 
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.u
 
 mongoose.set("strictQuery", false);
 
@@ -21,6 +23,32 @@ app.get('/', (req, res) =>{
     res.json({this: 'is a test, this should be a homepage'})
     })
 
-app.get('/api/albums', async (req,res) => {   // GET ALL
-    res.json(await db.find())
+app.get('/api/albums', async (req,res) => {   // GET all album data
+    try {
+      res.status(200).json(await db.Album.find())
+    }
+    catch(error){
+      res.status(404).json({})
+    }
 })
+
+app.get('/api/albums/:title', async (req,res) => {   // POST route to get specific book
+  try
+  {
+    var data = await db.Album.find({title: req.params.title})
+    if (data.length == 0){
+      res.status(404).json({error: 'Album not found'})
+  } // if null return error and code 404
+    
+  else
+  {    res.status(200).json()  // Model.find() returns even documents with duplicate album title
+
+  }  
+}
+  catch(error){
+    res.json({error})
+  }
+})
+
+app.listen(3000)
+// TODO: Prepared statements for input
